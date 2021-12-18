0c39bd45a62ceaa2297b5b6126aefc8854c8df25
yahya <crazy20046@gmail.com>
2021-12-18 08:05:41 +0200

    Update my_list.rb

---
+++ my_list.rb
@@ -0,0 +1,22 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
+end
+
+
+# Test #filter
+irb> list.filter {|e| e.even?}
+=> [2, 4]
